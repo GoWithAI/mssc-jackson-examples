637e91c148266d2118c59261142c394aac6efb57
deepak <dipakpatilcse@gmail.com>
2020-08-26 12:29:15 +0530

    Jackson Examples

---
+++ src/test/java/com/deep/msscjacksonexample/MsscjacksonexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deep.msscjacksonexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscjacksonexampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/deep/msscjacksonexample/model/BeerDtoSnakeTest.java
@@ -0,0 +1,8 @@
+package com.deep.msscjacksonexample.model;
+
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+
+
+@JsonTest
+public class BeerDtoSnakeTest extends BaseTest{
+}
